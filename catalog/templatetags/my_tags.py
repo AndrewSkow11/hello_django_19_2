cff4ad644a8f7e723891fe6c633030a1e9bcf79e
Andrew Skow <skovorodnikov11@gmail.com>
2024-02-17 12:07:05 +0300

    Исправлен %20

--- catalog/templatetags/my_tags.py
+++ catalog/templatetags/my_tags.py
@@ -7,6 +7,6 @@ register = template.Library()
 @register.filter()
 def show_img(img_url):
     if img_url:
-        return f'/%20/media/{img_url}'
+        return f'/media/{img_url}'
     return "https://otvet.imgsmail.ru/download/209827595_062753955e431ed09b9ea0bb74716df2_800.jpg"
 
