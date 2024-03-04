5a543b3d89d5db43bda2172afb79392fc6fd2cc8
Andrew Skow <skovorodnikov11@gmail.com>
2024-03-04 19:27:16 +0300

    22_1 Реализован запрет на добавление стоп-слов

--- catalog/forms.py
+++ catalog/forms.py
@@ -16,3 +16,28 @@ class ProductForm(forms.ModelForm):
         #           'manufactured_at')
         # exclude =  ('manufactured_at', )
 
+    stop_words = ['казино', 'криптовалюта', 'крипта', 'биржа', 'дешево',
+                  'бесплатно', 'обман', 'полиция', 'радар']
+
+    def clean_nomination(self):
+        nomination = self.cleaned_data['nomination']
+        for word in self.stop_words:
+            if word in nomination:
+                raise forms.ValidationError(f'Запрещено использовать {self.stop_words}')
+
+        return nomination
+
+    # def clean_nomination(self):
+    #     cleaned_data = self.cleaned_data.get('nomination')
+    #
+    #     if '1234' not in cleaned_data:
+    #         raise forms.ValidationError('Очень плохое слово')
+
+            # for word in self.stop_words:
+            #     if word in cleaned_data:
+            #         raise forms.ValidationError('Ипользовано запрещённое слово')
+
+        return cleaned_data
+
+        # def clean_description(self):
+
