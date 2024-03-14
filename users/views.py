dfd5eaf3ef81c2807efdc1b0ca4f0c0689888b81
Andrew Skow <skovorodnikov11@gmail.com>
2024-03-14 18:43:57 +0300

    сделана смена пароля

--- users/views.py
+++ users/views.py
@@ -46,10 +46,10 @@ class UserForgotPasswordView(SuccessMessageMixin, PasswordResetView):
     """
     form_class = UserForgotPasswordForm
     template_name = 'users/user_password_reset.html'
-    success_url = reverse_lazy('home')
+    success_url = reverse_lazy('catalog:home')
     success_message = 'Письмо с инструкцией по восстановлению пароля отправлена на ваш email'
-    subject_template_name = 'system/email/password_subject_reset_mail.txt'
-    email_template_name = 'system/email/password_reset_mail.html'
+    subject_template_name = 'users/password_subject_reset_mail.txt'
+    email_template_name = 'users/password_reset_mail.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -63,7 +63,7 @@ class UserPasswordResetConfirmView(SuccessMessageMixin, PasswordResetConfirmView
     """
     form_class = UserSetNewPasswordForm
     template_name = 'users/user_password_set_new.html'
-    success_url = reverse_lazy('home')
+    success_url = reverse_lazy('catalog:home')
     success_message = 'Пароль успешно изменен. Можете авторизоваться на сайте.'
 
     def get_context_data(self, **kwargs):
